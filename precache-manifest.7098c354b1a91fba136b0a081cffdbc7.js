3bed72929877ba60394a7a8688d21bae2719da52
katesivy <58646769+katesivy@users.noreply.github.com>
2020-04-02 15:08:07 -0400

    Updates

---
+++ precache-manifest.7098c354b1a91fba136b0a081cffdbc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d711ee0e51e192de9c2001a5aa7051",
+    "url": "/Restaurant/index.html"
+  },
+  {
+    "revision": "f59d09f316b1dacdfa5c",
+    "url": "/Restaurant/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "74dbef7ca28be4ef6b81",
+    "url": "/Restaurant/static/css/main.dd224954.chunk.css"
+  },
+  {
+    "revision": "f59d09f316b1dacdfa5c",
+    "url": "/Restaurant/static/js/2.076c467c.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/Restaurant/static/js/2.076c467c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74dbef7ca28be4ef6b81",
+    "url": "/Restaurant/static/js/main.6111eab1.chunk.js"
+  },
+  {
+    "revision": "19b0d7ff5be19b8d0b7b",
+    "url": "/Restaurant/static/js/runtime-main.43ce4615.js"
+  }
+]);
